2862fd74aec8c84a788e20ed5d1e8d3f1c2f310b
Rysyq <wikto-ol@wp.pl>
2024-06-16 14:13:32 +0200

    Remaking level to make it work better

--- Level.cs
+++ Level.cs
@@ -1,47 +1,137 @@
 public class Level
 {
-    private string[] levelData;
+    public Coords Origin { get; set;}
+    public Coords Size { get; }
+
+    private int[][] levelData;
+    private Dictionary<CellTypes, char> cellVisuals = new Dictionary<CellTypes, char>{
+        { CellTypes.WallCorner, '■'},
+        { CellTypes.WallHorizontal, '═'},
+        { CellTypes.WallVertical, '║'},
+        { CellTypes.Grass, '.'},
+        { CellTypes.Empty, ' '},
+        { CellTypes.Cross, '†'},
+    };
+
+    //stąd: https://pl.piliapp.com/symbol/square/
+    
+
+    private CellTypes[] walkableCellTypes = new CellTypes[] { 
+        CellTypes.Grass, 
+        CellTypes.Cross,
+    };
 
     public Level()
     {
-        levelData = new string[] {
-"╔═════════════╦════════════════════════════════════════════════╦══════════════════════════════════════════════════════╗",
-"║             ║                                                ║                                                      ║",
-"║             ║                                                ║                                                      ║",
-"║             ║                                                ║                                                      ║",
-"║             ║                                                ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                                                                                     ║",
-"║                                                                                                                     ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"║                                                              ║                                                      ║",
-"╚══════════════════════════════════════════════════════════════╩══════════════════════════════════════════════════════╝"
-};
+        levelData = new int[][] {
+            new []{1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,4,4,0,0,0,4,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,},
+            new []{1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,},
+        };
+
+        int y = levelData.Length;
+        int x = 0;
+
+        foreach (int[] row in levelData)
+        {
+            if (row.Length > x)
+            {
+                x = row.Length;
+            }
+        }
+
+        Size = new Coords(x, y);
+        Origin = new Coords(0, 0);
     }
 
-     public void Display()
+    public CellTypes GetCellAt(Coords Coords)
     {
-        foreach (string row in levelData)
+        return GetCellAt(Coords.X, Coords.Y);
+    }
+
+    private CellTypes GetCellAt(int x, int y)
+    {
+        return (CellTypes)levelData[y][x];
+    }
+
+    public char GetCellVisualAt(Coords Coords)
+    {
+        return cellVisuals[GetCellAt(Coords)];
+    }
+
+    public void Display(Coords origin)
+    {
+        Origin = origin;
+        Console.CursorTop = origin.Y;
+        for (int y = 0; y < levelData.Length; y++)
         {
-            Console.WriteLine(row);
+            Console.CursorLeft = origin.X;
+            for (int x = 0; x < levelData[y].Length; x++)
+            {
+                var cellValue = GetCellAt(x, y);
+                var cellVisual = cellVisuals[cellValue];
+                Console.Write(cellVisual);
+                Console.ResetColor();
+            }
+            Console.WriteLine();
         }
     }
 
-    public char GetCellAt(Coordinates coords)
-    {
-        string previousRow = levelData[coords.Y];
-        char previousCell = previousRow[coords.X];
 
-        return previousCell;
+    internal bool IsCoordsCorrect(Coords Coords)
+    {
+        if (Coords.Y >= 0 && Coords.Y < levelData.Length)
+        {
+            if (Coords.X >= 0 && Coords.X < levelData[Coords.Y].Length)
+            {
+                if (walkableCellTypes.Contains(GetCellAt(Coords)))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    internal void DrawSomethingAt(char visual, Coords position)
+    {
+        Console.SetCursorPosition(position.X + Origin.X, position.Y + Origin.Y);
+        Console.Write(visual);
+    }
+
+    internal void DrawSomethingAt(string visual, Coords position)
+    {
+        Console.SetCursorPosition(position.X + Origin.X, position.Y + Origin.Y);
+        Console.Write(visual);
+    }
+
+
+    internal void RedrawCellAt(Coords position)
+    {
+        var cellValue = GetCellAt(position);
+        var cellVisual = GetCellVisualAt(position);
+        DrawSomethingAt(cellVisual, position);
+        Console.ResetColor();
     }
 }
