e1210cb300e05f22194230ade782e8650b5041bc
Rysyq <wikto-ol@wp.pl>
2024-06-16 12:36:34 +0200

    Change idea about that how should work level.cs - creating "symbol list" or whatever is it

---
+++ CellTypes.cs
@@ -0,0 +1,14 @@
+public enum CellTypes
+{
+    //with coll
+    WallCorner = 1,
+    WallHorizontal = 2,
+    WallVertical = 3,
+
+    //without coll
+    Grass = 0,
+    Cross = 4,
+    Empty = 5,
+}
+
+//stąd: https://pl.piliapp.com/symbol/square/
