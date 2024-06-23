5c12f18440a070e5678c11d761fe7b70ff58f275
Rysyq <wikto-ol@wp.pl>
2024-06-23 20:20:41 +0200

    Adding all to program

--- Program.cs
+++ Program.cs
@@ -2,146 +2,39 @@
 using System.Runtime.CompilerServices;
 using System.Security;
 
-Player player = new Player(3, 4);
+Console.CursorVisible = false;
+
+Coords playerPosition = new Coords(10, 3);
+
+ComposedPlayer composedPlayer = new ComposedPlayer("S", playerPosition);
+
 
 Level level = new Level();
-
+Coords levelOrigin = new Coords(15, 3);
 Console.Clear();
+ConsoleKeyInfo pressedKey = Console.ReadKey(true);
 
-level.Display();
 
-Console.SetCursorPosition(player.LastCoords.X, player.LastCoords.Y);
-Console.Write("@");
 
-Console.CursorVisible = false;
+level.Display(levelOrigin);
+
+level.DrawSomethingAt(composedPlayer.VisualComp.Visual, composedPlayer.PositionComp.Position);
+
+
 while (true)
 {
-    Coordinates nextCoords = player.GetNextCoords();
+    if (pressedKey.Key == ConsoleKey.K)
+    {
 
-    player.Move(nextCoords);
+        Console.WriteLine("Damage!");
 
+    }
+    Coords nextPosition = composedPlayer.Move.GetNextPosition();
+    if (level.IsCoordsCorrect(nextPosition))
+    {
+        composedPlayer.Move.Move(nextPosition);
 
-    char previousCell = level.GetCellAt(player.PreviousCoords);
-    Console.SetCursorPosition(player.PreviousCoords.X, player.PreviousCoords.Y);
-    Console.Write(previousCell);
+        level.RedrawCellAt(composedPlayer.Move.PreviousPosition);
+        level.DrawSomethingAt(composedPlayer.VisualComp.Visual, composedPlayer.PositionComp.Position);
+    }
 }
-
-
-// void UpdatePos()
-// {
-//     player.Move();
-
-
-
-//     // if (logicChars.Contains(level[x][y]))
-//     // {
-//     //     Console.Clear();
-//     //     Console.WriteLine("You Won !!!");
-//     // }
-//     // else if (!(obstacle.Contains(level[x][y])))
-//     // {
-//     //     Console.SetCursorPosition(oldY, oldX);
-//     //     Console.Write(" ");
-
-//     //     Console.SetCursorPosition(y, x);
-//     //     Console.Write("S");
-//     // }
-//     // else 
-//     // {
-//     //     x = oldX;
-//     //     y = oldY;
-//     // }
-// }
-
-//aby zrobić to poniżej - coordy, coordy do movementu - przerobienie poruszania 
-//poruszanie done > pozycja, a mapa, żeby nie zżerało - map tu może być, I quess 
-//skoro i tak jest potrzebny X i Y, aby była poprzednia pozycja to równie dobrze można wrzucić to w osobną klasę
-
-//o co chodzi dół >
-
-// /int row = 1;
-// int col = 1;
-// int oldRow;
-// int oldCol;
-// ConsoleKey playerInput;
-// string obstacle = "WP5";
-// string logicChars = "A";
-
-// string[] map =
-// {
-//     "WWWWWWWWW",
-//     "W     A W",
-//     "W       W",
-//     "W   WWWWW",
-//     "W   W",
-//     "W   W",
-//     "WWWWW",
-
-// };
-// InitializeMap(map);
-// InitializeplayerAvatar();
-// Console.CursorVisible = false;
-
-// while (true)
-// {
-//     oldCol = col;
-//     oldRow = row;
-
-//     playerInput = Console.ReadKey(true).Key;
-//     UpdatePos(playerInput);
-
-
-
-// }
-
-// void UpdatePos(ConsoleKey toCoNacisnalGracz)
-// {
-//     switch (toCoNacisnalGracz)
-//     {
-//         case ConsoleKey.W: row--; break;
-//         case ConsoleKey.S: row++; break;
-//         case ConsoleKey.A: col--; break;
-//         case ConsoleKey.D: col++; break;
-//     }
-
-//     if (logicChars.Contains(map[row][col]))
-//     {
-//         Console.Clear();
-//         Console.WriteLine("You Won !!!");
-//     }
-//     else if (!(obstacle.Contains(map[row][col])))
-//     {
-//         Console.SetCursorPosition(oldCol, oldRow);
-//         Console.Write(" ");
-
-//         Console.SetCursorPosition(col, row);
-//         Console.Write("#");
-//     }
-//     else 
-//     {
-//         row = oldRow;
-//         col = oldCol;
-//     }
-
-// }
-
-// void Movement()
-// {
-
-// }
-
-// void InitializeplayerAvatar()
-// {
-//     Console.SetCursorPosition(col, row);
-//     Console.Write("#");
-
-// }
-
-// void InitializeMap(string[] mapToPrint)
-// {
-//     Console.Clear();
-//     foreach( string row in mapToPrint)
-//     {
-//         Console.WriteLine(row);
-//     }
-// }
