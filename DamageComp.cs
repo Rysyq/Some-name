429134e47cbc79b36c59ba959323a6b3dc926026
Rysyq <wikto-ol@wp.pl>
2024-06-24 13:52:37 +0200

    Adding one more option to damage

--- DamageComp.cs
+++ DamageComp.cs
@@ -3,11 +3,13 @@ internal class DamageComp
     public int Dmg
     {
         get => basicdmg;
-        set => basicdmg = Math.Clamp(value, 5, 120);
+        set => basicdmg = Math.Clamp(value, 5, 100);
     }
-    int basicdmg = 5;
-    int MaxDmg { get; set; } = 120;
+    int basicdmg = 10;
+    int MaxDmg { get; set; } = 100;
     int range = 2;
+
+    int enemyrange = 1;
     private readonly PositionComp positionComp;
 
     public DamageComp(PositionComp positionComp)
@@ -21,6 +23,14 @@ internal class DamageComp
         int distanceY = Math.Abs(positionComp.Position.Y - targetPosition.Y);
 
         return (distanceX <= range && distanceY == 0) || (distanceX == 0 && distanceY <= range);
+    }
+
+    public bool CanEnemyHitInThisRange(Coords targetPosition)
+    {
+        int distanceX = Math.Abs(positionComp.Position.X - targetPosition.X);
+        int distanceY = Math.Abs(positionComp.Position.Y - targetPosition.Y);
+
+        return (distanceX <= enemyrange && distanceY == 0) || (distanceX == 0 && distanceY <= enemyrange);
     }
 
     public void Attack(HealthComp targetHealthComp)
